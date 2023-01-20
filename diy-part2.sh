79d99a8269e6d257bc47339a20152216cb355bf3
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2023-01-20 11:01:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,3 +27,4 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/psw
 git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/pswcore
+git clone https://github.com/sirpdboy/netspeedtest package/netspeedtest
